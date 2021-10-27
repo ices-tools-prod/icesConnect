890c296f3a9de6e57217f497d541fadaccbd84c7
Colin Millar <colin.millar@ices.dk>
2021-10-27 15:21:24 +0200

    add decode token function

--- R/decode_token.R
+++ R/decode_token.R
@@ -1,6 +1,28 @@
-
+#' Decode token
+#'
+#' Decodes the token to allow inspection of claims
+#'
+#' @param jwt a javascript web token got by running `ices_token()`
+#'
+#' @return list of claims
+#'
+#' @examples
+#' \dontrun{
+#' jwt <- ices_token()
+#' decode_token(jwt)
+#' }
+#'
+#' @seealso
+#'  \code{\link[whoami]{username}}
+#'  \code{\link[keyring]{key_get}}
+#'
+#' @rdname decode_token
+#'
 #' @importFrom base64enc base64decode
 #' @importFrom jsonlite parse_json
+#'
+#' @export
+
 
 decode_token <- function(jwt) {
 
